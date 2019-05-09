958e3c6662e6bf3a804d354ff47f9533f4672df7
engintruder <engkwangseob@gmail.com>
2019-05-09 13:05:43 +0900

    0509

---
+++ ecma_ex2/0_js.js
@@ -0,0 +1,26 @@
+/*
+var es5 = function(one, two) {
+    return one + two;
+}
+
+var sum = es5(1, 2);
+console.log(sum);
+*/
+
+/*
+let es6 = (one, two)  => {
+    return one + two;
+};
+let result = es6(1, 2);
+console.log(result);
+*/
+
+/*
+let total = (one, two) => one + two;
+let result = total(1, 2);
+console.log(result);
+*/
+let total = (one, two) => 
+one + two;
+let result = total(1, 2);
+console.log(result);
---
+++ ecma_ex2/1_js.js
@@ -0,0 +1,21 @@
+/*
+let arrayObj = [];
+let result = arrayObj[Symbol.iterator];
+console.log(result);
+*/
+
+/*
+let objectObj = {};
+let result = objectObj[Symbol.iterator];
+console.log(result);
+*/
+
+/*
+let arrayObj = [1, 2];
+let iteratorObj = arrayObj[Symbol.iterator]();
+console.log(“1: ”, typeof iteratorObj);
+
+console.log(“2: ”, iteratorObj.next());
+console.log(“3: ”, iteratorObj.next());
+console.log(“4: ”, iteratorObj.next());
+*/
